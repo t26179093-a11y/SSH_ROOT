efe2231ef7c311f3075dbefd0a4521d7a86d16bf
t26179093-a11y <t26179093@gmail.com>
2025-10-24 22:48:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,4 @@
 service ssh start
 
 echo "Starte tmate..."
-tmate -F &
-sleep 5
-
-# Link anzeigen (Web & SSH)
-tmate display -p '#{tmate_ssh}'
-tmate display -p '#{tmate_web}'
-
-# 24/7 laufen lassen
-tail -f /dev/null
+tmate -F new-session
