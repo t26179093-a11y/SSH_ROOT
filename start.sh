6e0d16df15468d12d501b7c061364b520ea77677
t26179093-a11y <t26179093@gmail.com>
2025-10-25 00:12:34 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-
-# SSH starten (im Vordergrund, damit Docker nicht stoppt)
-mkdir -p /var/run/sshd
-
-# Root Login sicherstellen
-echo "root:root" | chpasswd
-
-# SSH Server starten im Vordergrund
-/usr/sbin/sshd -D -e &
-
-# IP-Adresse anzeigen
-IP_ADDR=$(hostname -I | awk '{print $1}')
-
 echo "==============================="
-echo "SSH Server läuft!"
-echo "IP-Adresse: $IP_ADDR"
-echo "Benutzer: root"
-echo "Passwort: root"
-echo "Port: 22"
+echo "Starting SSH + WebSSH (tmate)..."
+echo "User: root"
+echo "Password: root"
+echo "Port: 22 (internal)"
 echo "==============================="
 
-# Logs direkt aus stdout (Docker log)
-echo "SSH Logs werden in Docker-Console angezeigt."
-wait
+# SSH starten
+/usr/sbin/sshd
+
+# tmate Session starten (WebSSH)
+tmate -F
